68b1a43a2c231e17b20538025b467dab1940b425
최미래 <miraexhoi@gmail.com>
2023-08-08 14:26:41 +0900

    8/8 class

--- src/test/java/org/zerock/ex2/sample/SampleTests.java
+++ src/test/java/org/zerock/ex2/sample/SampleTests.java
@@ -1,20 +1,45 @@
 package org.zerock.ex2.sample;
 
+import lombok.ToString;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SampleTests {
 
+    @Autowired // DI
     private Restaurant restaurant;
 
+
+
+//    @Test
+//    public void test1() {
+////        restaurant = new Restaurant();
+//        System.out.println("================");
+//        System.out.println(restaurant);
+////        restaurant.test();
+//        System.out.println("================");
+//    }
+//
+//    public void test2(){
+//        Member member1;
+//        member1 = new Member("홍길동",20);
+//        System.out.println(member1);
+//        member1 = new Member("홍길동",22);
+//        System.out.println(member1);
+//    }
+
     @Test
-    public void test() {
-        restaurant = new Restaurant();
-        System.out.println("================");
-        System.out.println(restaurant);
-        restaurant.test();
-        System.out.println("================");
+    public void test3() {
+        Member member1 = new Member();
+        Member member2 = new Member("홍길동",20);
+//        member.setName("홍길동");
+//        member.setAge(20);
+        System.out.println(member1);
+        System.out.println(member2);
     }
 }
