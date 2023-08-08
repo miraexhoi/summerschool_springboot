3bb3e5cf5059d356b24499bbd71a7be2372a9fb4
최미래 <miraexhoi@gmail.com>
2023-08-08 14:27:29 +0900

    8/8 class

---
+++ src/main/java/org/zerock/ex2/sample/Member.java
@@ -0,0 +1,34 @@
+package org.zerock.ex2.sample;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String name;
+    private int age;
+
+//    public Member(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+}
