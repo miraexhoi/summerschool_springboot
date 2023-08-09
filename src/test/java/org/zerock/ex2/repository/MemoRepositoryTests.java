3b08df326953c8198f096811e67cf66976a70b0a
최미래 <miraexhoi@gmail.com>
2023-08-09 15:18:54 +0900

    8/9 class

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.ex2.entity.Memo;
 
 import java.util.List;
@@ -59,5 +63,46 @@ public class MemoRepositoryTests {
     public void testDelete(){
         Long mno = 100L;
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testDefault(){
+        Pageable pageable
+                = PageRequest
+                .of(1,10);
+        Page<Memo> result = memoRepository
+                .findAll(pageable);
+        result.stream().forEach(memo -> {
+            System.out.println(memo);
+        });
+        System.out.println("———————————————————");
+        System.out.println("Total Pages: " + result.getTotalPages()); //총 몇 페이지
+        System.out.println("Total Count: " + result.getTotalElements()); //전체 개수
+
+        System.out.println("Page Number: " + result.getNumber()); //현재 페이지 번호 0부터 시작
+        System.out.println("Page Size: " + result.getSize()); //페이지당 데이터 개수
+        System.out.println("has next page?: " + result.hasNext()); //다음 페이지 존재 여부
+        System.out.println("first page?: " + result.isFirst()); //시작 페이지(0) 여부
+    }
+    @Test
+    public void testSort(){
+        Sort sort = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0,10, sort);
+        Page<Memo> list = memoRepository.findAll(pageable);
+        list.get().forEach(memo -> {
+            System.out.println(memo);
+        });
+    }
+//    @Test
+//    public void testQueryMethod1() {
+//        List<Memo> list = memoRepository
+//                .findByMnoBetweenOrderByMnoDesc(50L, 60L);
+//        list.stream().forEach(memo -> System.out.println(memo));
+//    }
+    @Test
+    public void testQueryMethod2() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+        Page<Memo> result = memoRepository.findByMnoBetween(50L, 60L, pageable);
+        result.stream().forEach(memo -> System.out.println(memo));
     }
 }
