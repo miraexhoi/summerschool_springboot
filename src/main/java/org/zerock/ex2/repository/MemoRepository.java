bb46f7fd26d33932d16999e4030f79e23156687a
최미래 <miraexhoi@gmail.com>
2023-08-09 15:19:02 +0900

    8/9 class

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,8 +1,19 @@
 package org.zerock.ex2.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ex2.entity.Memo;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import java.awt.print.Pageable;
+import java.util.List;
 
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+//    Between
+//    findByStartDateBetween
+//    … where x.startDate between ?1 and ?2
+    List<Memo> findByMnoBetween(Long from, Long to, org.springframework.data.domain.Pageable pageable);
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(long from, long to, Pageable pageable);
+
+    Page<Memo> findByMnoBetween(long l, long l1, org.springframework.data.domain.Pageable pageable);
 }
