00c13d0178729a2919ec0aa48e0024d7406ccfe6
최미래 <miraexhoi@gmail.com>
2023-08-09 11:46:19 +0900

    8/9 class

--- src/main/java/org/zerock/ex2/entity/Memo.java
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -1,9 +1,16 @@
 package org.zerock.ex2.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tbl_memo")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Memo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AI
