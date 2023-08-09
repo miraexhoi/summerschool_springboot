25924b818a6ca91e2517752f3d648dad18e233c4
최미래 <miraexhoi@gmail.com>
2023-08-09 09:50:52 +0900

    8/9 class

---
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,15 @@
+package org.zerock.ex2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+    @Autowired
+    private MemoRepository memoRepository;
+    @Test
+    public void testInsertDummies(){
+
+    }
+}
